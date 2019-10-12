bdf87143745fe5631607569ac22bda0b351dd495
sharmaanand086 <38725515+sharmaanand086@users.noreply.github.com>
2019-10-12 15:39:52 +0530

    Add files via upload

---
+++ php_shopping_cart/index.php
@@ -0,0 +1,151 @@
+<?php 
+// Initialize shopping cart class 
+include_once 'Cart.class.php'; 
+$cart = new Cart; 
+ 
+// Include the database config file 
+require_once 'dbConfig.php'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>PHP Shopping Cart</title>
+<meta charset="utf-8">
+
+<!-- Bootstrap core CSS -->
+<link href="http://demos.codexworld.com/includes/css/bootstrap.css" rel="stylesheet">
+
+<!-- Custom style -->
+<link href="http://demos.codexworld.com/includes/css/style.css" rel="stylesheet">
+
+</head>
+</head>
+<body>
+<div class="container">
+    <h1>PRODUCTS</h1>
+    
+    <!-- Cart basket -->
+    <div class="cart-view">
+        <a href="viewCart.php" title="View Cart"><i class="icart"></i> (<?php echo ($cart->total_items() > 0)?$cart->total_items().' Items':'Empty'; ?>)</a>
+    </div>
+    
+    <!-- Product list -->
+    <div class="row col-lg-12">
+        <?php 
+        // Get products from database 
+        $result = $db->query("SELECT * FROM products ORDER BY id DESC LIMIT 10"); 
+        if($result->num_rows > 0){  
+            while($row = $result->fetch_assoc()){ 
+        ?>
+        <div class="card col-lg-4">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $row["name"]; ?></h5>
+                <h6 class="card-subtitle mb-2 text-muted">Price: <?php echo '$'.$row["price"].' USD'; ?></h6>
+                <p class="card-text"><?php echo $row["description"]; ?></p>
+                <a href="cartAction.php?action=addToCart&id=<?php echo $row["id"]; ?>" class="btn btn-primary">Add to Cart</a>
+            </div>
+        </div>
+        <?php } }else{ ?>
+        <p>Product(s) not found.....</p>
+        <?php } ?>
+    </div>
+</div>
+</body>
+</html><?php 
+// Initialize shopping cart class 
+include_once 'Cart.class.php'; 
+$cart = new Cart; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>View Cart - PHP Shopping Cart</title>
+<meta charset="utf-8">
+
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom style -->
+<link href="css/style.css" rel="stylesheet">
+
+<!-- jQuery library -->
+<script src="js/jquery.min.js"></script>
+
+<script>
+function updateCartItem(obj,id){
+    $.get("cartAction.php", {action:"updateCartItem", id:id, qty:obj.value}, function(data){
+        if(data == 'ok'){
+            location.reload();
+        }else{
+            alert('Cart update failed, please try again.');
+        }
+    });
+}
+</script>
+</head>
+<body>
+<div class="container">
+    <h1>SHOPPING CART</h1>
+    <div class="row">
+        <div class="cart">
+            <div class="col-12">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th width="45%">Product</th>
+                                <th width="10%">Price</th>
+                                <th width="15%">Quantity</th>
+                                <th class="text-right" width="20%">Total</th>
+                                <th width="10%"> </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                            if($cart->total_items() > 0){ 
+                                // Get cart items from session 
+                                $cartItems = $cart->contents(); 
+                                foreach($cartItems as $item){ 
+                            ?>
+                            <tr>
+                                <td><?php echo $item["name"]; ?></td>
+                                <td><?php echo '$'.$item["price"].' USD'; ?></td>
+                                <td><input class="form-control" type="number" value="<?php echo $item["qty"]; ?>" onchange="updateCartItem(this, '<?php echo $item["rowid"]; ?>')"/></td>
+                                <td class="text-right"><?php echo '$'.$item["subtotal"].' USD'; ?></td>
+                                <td class="text-right"><button class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')?window.location.href='cartAction.php?action=removeCartItem&id=<?php echo $item["rowid"]; ?>':false;"><i class="itrash"></i> </button> </td>
+                            </tr>
+                            <?php } }else{ ?>
+                            <tr><td colspan="5"><p>Your cart is empty.....</p></td>
+                            <?php } ?>
+                            <?php if($cart->total_items() > 0){ ?>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td><strong>Cart Total</strong></td>
+                                <td class="text-right"><strong><?php echo '$'.$cart->total().' USD'; ?></strong></td>
+                                <td></td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col mb-2">
+                <div class="row">
+                    <div class="col-sm-12  col-md-6">
+                        <a href="index.php" class="btn btn-block btn-light">Continue Shopping</a>
+                    </div>
+                    <div class="col-sm-12 col-md-6 text-right">
+                        <?php if($cart->total_items() > 0){ ?>
+                        <a href="checkout.php" class="btn btn-lg btn-block btn-primary">Checkout</a>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ php_shopping_cart/shopping.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 12, 2019 at 12:06 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shopping`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `address` text COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1' COMMENT '1=Active | 0=Inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `first_name`, `last_name`, `email`, `phone`, `address`, `created`, `modified`, `status`) VALUES
+(1, 'anand', 'sharma', 'bhavesh@arfeenkhan.com', '09874563210', 'adfsjfkjo', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '1'),
+(2, 'anand', 'sharma', 'bhavesh@arfeenkhan.com', '09874563210', 'adfsjfkjo', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '1'),
+(3, 'sfa', 'asdfa', 'asdfaafsdaf@sgfs', 'sgfsdg', 'sdgfdsg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `grand_total` float(10,2) NOT NULL,
+  `created` datetime NOT NULL,
+  `status` enum('Pending','Completed','Cancelled') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer_id`, `grand_total`, `created`, `status`) VALUES
+(1, 1, 50.00, '2019-10-12 14:57:03', 'Pending'),
+(2, 2, 70.00, '2019-10-12 15:20:53', 'Pending'),
+(3, 3, 25.00, '2019-10-12 15:33:59', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`) VALUES
+(1, 1, 1, 2),
+(2, 2, 2, 1),
+(3, 2, 1, 2),
+(4, 3, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `price` float(10,2) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1' COMMENT '1=Active | 0=Inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `created`, `modified`, `status`) VALUES
+(1, 'Product 2', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 25.00, '2019-10-12 00:00:00', '2019-10-12 00:00:00', '1'),
+(2, 'Product 1', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', 20.00, '2019-10-12 00:00:00', '2019-10-12 00:00:00', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
